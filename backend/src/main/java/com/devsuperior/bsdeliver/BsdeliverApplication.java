7c19beca0947753265078a5771edf89f58c18e3f
Bruno Soares <brunosoaresbsv@gmail.com>
2021-01-04 22:43:38 -0300

    Project bsdeliver created

---
+++ backend/src/main/java/com/devsuperior/bsdeliver/BsdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.bsdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsdeliverApplication.class, args);
+	}
+
+}
