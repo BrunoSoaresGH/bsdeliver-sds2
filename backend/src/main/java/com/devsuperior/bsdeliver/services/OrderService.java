5a0d99a90f247438272cf19b21b055e9069f0c42
Bruno Soares <brunosoaresbsv@gmail.com>
2021-01-07 20:22:51 -0300

    GET /orders

---
+++ backend/src/main/java/com/devsuperior/bsdeliver/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.bsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.bsdeliver.dto.OrderDTO;
+import com.devsuperior.bsdeliver.entities.Order;
+import com.devsuperior.bsdeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll(){
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+	
+}
