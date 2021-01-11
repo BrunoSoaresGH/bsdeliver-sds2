1a2313d0ed93b49449a6e96de2f863b403cedfc9
Bruno Soares <brunosoaresbsv@gmail.com>
2021-01-11 09:01:59 -0300

    Add Routes and StepsHeader

--- front-web/src/NavBar/index.tsx
+++ front-web/src/NavBar/index.tsx
@@ -1,11 +1,12 @@
 import './styles.css';
 import Logo from './logo.png'; 
+import { Link } from 'react-router-dom';
 
 function NavBar(){
     return (
         <nav className="main-navbar">
             <img src={Logo} alt="BS Deliver"/>
-            <a href="home" className="logo-text">BS Deliver</a>
+            <Link to="/" className="logo-text">BS Deliver</Link>
         </nav>
     )
 }
